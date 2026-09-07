a64bdff00901f679ea3f7ab056877a7338742aa1
abhijitreddy-06 <abhijitreddy828@gmail.com>
2026-09-08 01:02:10 +0530

    ss

--- src/notifications/notification.scheduler.js
+++ src/notifications/notification.scheduler.js
@@ -90,7 +90,7 @@ export const runPushReceiptCheckJob = async () => {
 export const startNotificationScheduler = () => {
   if (schedulerStarted) return;
   schedulerStarted = true;
-  cron.schedule("10 1 * * *", () => void runHabitReminderJob(), {
+  cron.schedule("5 1 * * *", () => void runHabitReminderJob(), {
     timezone: IST_TIME_ZONE,
   });
   cron.schedule("0 23 * * *", () => void runBirthdayReminderJob(), {
